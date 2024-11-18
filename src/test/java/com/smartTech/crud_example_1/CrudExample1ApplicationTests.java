2bda46cfdfd0c8fa263c69c42300a93145808862
ThumulaR <thumular@kingslake.com>
2024-11-18 10:05:52 +0530

    Initial Commit

---
+++ src/test/java/com/smartTech/crud_example_1/CrudExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartTech.crud_example_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
