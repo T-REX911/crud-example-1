1f632f947fef849eb114f2693a79774f15513662
ThumulaR <thumular@kingslake.com>
2024-11-25 16:11:32 +0530

    read RequestHeader & tenant

--- src/main/java/com/smartTech/crud_example_1/controller/HelloController.java
+++ src/main/java/com/smartTech/crud_example_1/controller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
 
   @GetMapping("")
   public ResponseEntity<String> helloTenant (@RequestHeader("tenant_id") String tenantId){
-    if (tenantId == null) {
+    if (tenantId == null || tenantId == "") {
       return ResponseEntity.badRequest().body("tenant_id header is missing");
     }
     return ResponseEntity.ok("Received tenant_id: " + tenantId);
