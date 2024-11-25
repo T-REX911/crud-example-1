7289b4f115e75acf1ea2c7d556f8153e1353f4bd
ThumulaR <thumular@kingslake.com>
2024-11-25 09:13:51 +0530

    Adding Constraints

--- src/main/java/com/smartTech/crud_example_1/entity/Student.java
+++ src/main/java/com/smartTech/crud_example_1/entity/Student.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Transactional
 @Data
@@ -19,4 +22,10 @@ public class Student {
   private int id;
   private String name;
   private String email;
+
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(name = "Enrollment",
+      joinColumns = @JoinColumn(name = "student_id"),
+      inverseJoinColumns = @JoinColumn(name = "course_id"))
+  private List<Course> courses = new ArrayList<>();
 }
